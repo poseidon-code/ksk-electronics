6e0eba9620cfb4c3de70ba93a21bd7060aca2aa6
poseidon-code <pritamhalder05062000@gmail.com>
2021-02-03 14:13:39 +0530

    update styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,61 +1,56 @@
-import { useState } from "react";
-import { NavLink } from "react-router-dom";
-import "./Header.scss";
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.scss';
 
-import { Close, Menu } from "../UI/Icons/Icons";
+import { Close, Menu } from '../UI/Icons/Icons';
 
 const Header = () => {
-  const [menu, toggleMenu] = useState(false);
+    const [menu, toggleMenu] = useState(false);
 
-  const toggleNavbar = () => {
-    const navbar = document.querySelector(".header .navbar_mobile");
-    if (navbar.style.display === "flex") {
-      navbar.style.display = "none";
-      toggleMenu(false);
-    } else {
-      navbar.style.display = "flex";
-      toggleMenu(true);
-    }
-  };
+    const toggleNavbar = () => {
+        toggleMenu((p) => !p);
+    };
 
-  return (
-    <section className="wrapper header">
-      <div className="container header">
-        <div className="brand">
-          <h1>ksk.electronics</h1>
-        </div>
-        <button className="navbar_toggle" onClick={toggleNavbar}>
-          {menu ? <Close /> : <Menu />}
-        </button>
-        <nav className="navbar">
-          <NavLink to="/" exact>
-            Home
-          </NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/courses">Courses</NavLink>
-          <NavLink to="/signin">Sign In</NavLink>
-          <NavLink to="/enroll">Enroll</NavLink>
-        </nav>
-        <nav className="navbar_mobile">
-          <NavLink to="/" exact onClick={toggleNavbar}>
-            Home
-          </NavLink>
-          <NavLink to="/about" onClick={toggleNavbar}>
-            About
-          </NavLink>
-          <NavLink to="/courses" onClick={toggleNavbar}>
-            Courses
-          </NavLink>
-          <NavLink to="/signin" onClick={toggleNavbar}>
-            Sign In
-          </NavLink>
-          <NavLink to="/enroll" onClick={toggleNavbar}>
-            Enroll
-          </NavLink>
-        </nav>
-      </div>
-    </section>
-  );
+    return (
+        <section className='wrapper header'>
+            <div className='container header'>
+                <div className='brand'>
+                    <h1>ksk.electronics</h1>
+                </div>
+                <button className='navbar_toggle' onClick={toggleNavbar}>
+                    {menu ? <Close /> : <Menu />}
+                </button>
+                <nav className='navbar'>
+                    <NavLink to='/' exact>
+                        Home
+                    </NavLink>
+                    <NavLink to='/about'>About</NavLink>
+                    <NavLink to='/courses'>Courses</NavLink>
+                    <NavLink to='/signin'>Sign In</NavLink>
+                    <NavLink to='/enroll'>Enroll</NavLink>
+                </nav>
+                {menu && (
+                    <nav className='navbar_mobile'>
+                        <NavLink to='/' exact onClick={toggleNavbar}>
+                            Home
+                        </NavLink>
+                        <NavLink to='/about' onClick={toggleNavbar}>
+                            About
+                        </NavLink>
+                        <NavLink to='/courses' onClick={toggleNavbar}>
+                            Courses
+                        </NavLink>
+                        <NavLink to='/signin' onClick={toggleNavbar}>
+                            Sign In
+                        </NavLink>
+                        <NavLink to='/enroll' onClick={toggleNavbar}>
+                            Enroll
+                        </NavLink>
+                    </nav>
+                )}
+            </div>
+        </section>
+    );
 };
 
 export default Header;
