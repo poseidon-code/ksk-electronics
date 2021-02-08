39a578fea0688d5eb793bd43a50630a820999255
poseidon-code <pritamhalder05062000@gmail.com>
2021-02-08 21:23:24 +0530

    add AdminDashboard

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
+import { useAuth } from './contexts/AuthContext';
 import './styles/App.scss';
 
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Courses from './pages/Courses';
 import Dashboard from './pages/Dashboard';
+import AdminDashboard from './pages/AdminDashboard';
 
 const App = () => {
+    const { isAdmin } = useAuth();
+
     return (
         <Layout>
             <Switch>
-                <PrivateRoute path='/dashboard' component={Dashboard} />
+                <PrivateRoute path='/dashboard' component={isAdmin ? AdminDashboard : Dashboard} />
                 <Route path='/' exact component={Home} />
                 <Route path='/courses' component={Courses} />
                 <Route path='*' exact>
---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+import '../styles/AdminDashboard.scss';
+
+const AdminDashboard = () => {
+    return (
+        <div className='admin-dashboard'>
+            <h1>Admin Dashboard</h1>
+        </div>
+    );
+};
+
+export default AdminDashboard;
